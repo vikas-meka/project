c770b3e34925662e8e8879b75274ae0c51888bfa
vikas-meka <phanindrajlk@gmail.com>
2024-11-06 16:11:25 +0530

    added admin key model tests

--- home/tests.py
+++ home/tests.py
@@ -3,22 +3,17 @@ from home import views
 from django.urls import reverse
 from .models import course, mark, student_detail, course_key, admin_key, grade, admin_detail, student_password
 
-class CourseKeyModelTests(TestCase):
-    """ Test cases for course_key model """
+class AdminKeyModelTests(TestCase):
+    """ Test cases for admin_key model """
 
     def setUp(self):
-        self.course_key = course_key.objects.create(key='key1', course='CS101', name='Data Structures')
+        self.admin_key = admin_key.objects.create(key='admin_key1', name='Admin 1')
 
-    def test_course_key_creation(self):
-        """ Test Case 6.1: Verify course key creation """
-        self.assertEqual(course_key.objects.count(), 1)
-        self.assertEqual(self.course_key.course, 'CS101')
+    def test_admin_key_creation(self):
+        """ Test Case 7.1: Verify admin key creation """
+        self.assertEqual(admin_key.objects.count(), 1)
+        self.assertEqual(self.admin_key.key, 'admin_key1')
     
-
-    def test_update_entered_marks(self):
-        """ Test Case 6.3: Update entered_marks """
-        self.course_key.entered_marks = '60'
-        self.course_key.save()
-        self.assertEqual(self.course_key.entered_marks, '60')
-
-
+    def test_admin_key_fields(self):
+        """ Test Case 7.2: Check optional fields """
+        self.assertIsNone(self.admin_key.entered_courses)
