22eee5de16bf87aaba5016c0ab1cf0f536fa6448
vikas-meka <phanindrajlk@gmail.com>
2024-11-06 16:09:08 +0530

    added course key model tests

--- home/tests.py
+++ home/tests.py
@@ -3,19 +3,22 @@ from home import views
 from django.urls import reverse
 from .models import course, mark, student_detail, course_key, admin_key, grade, admin_detail, student_password
 
-class GradeModelTests(TestCase):
-    """ Test cases for grade model """
+class CourseKeyModelTests(TestCase):
+    """ Test cases for course_key model """
 
     def setUp(self):
-        self.grade_entry = grade.objects.create(roll_no='ST001', cgpa='8.5')
+        self.course_key = course_key.objects.create(key='key1', course='CS101', name='Data Structures')
 
-    def test_grade_entry_creation(self):
-        """ Test Case 5.1: Verify grade entry creation """
-        self.assertEqual(grade.objects.count(), 1)
-        self.assertEqual(self.grade_entry.cgpa, '8.5')
+    def test_course_key_creation(self):
+        """ Test Case 6.1: Verify course key creation """
+        self.assertEqual(course_key.objects.count(), 1)
+        self.assertEqual(self.course_key.course, 'CS101')
     
-    def test_grade_string_representation(self):
-        """ Test Case 5.3: Verify __str__ representation """
-        self.assertEqual(str(self.grade_entry), 'ST001')
+
+    def test_update_entered_marks(self):
+        """ Test Case 6.3: Update entered_marks """
+        self.course_key.entered_marks = '60'
+        self.course_key.save()
+        self.assertEqual(self.course_key.entered_marks, '60')
 
 
