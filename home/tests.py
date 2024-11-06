1c03604c30aa63a74dfacf908725dc023e81108e
vikas-meka <phanindrajlk@gmail.com>
2024-11-06 16:13:10 +0530

    added student password model tests

--- home/tests.py
+++ home/tests.py
@@ -3,17 +3,18 @@ from home import views
 from django.urls import reverse
 from .models import course, mark, student_detail, course_key, admin_key, grade, admin_detail, student_password
 
-class AdminKeyModelTests(TestCase):
-    """ Test cases for admin_key model """
+class StudentPasswordModelTests(TestCase):
+    """ Test cases for student_password model """
 
     def setUp(self):
-        self.admin_key = admin_key.objects.create(key='admin_key1', name='Admin 1')
+        self.student_password = student_password.objects.create(username='student1', password='pass1')
 
-    def test_admin_key_creation(self):
-        """ Test Case 7.1: Verify admin key creation """
-        self.assertEqual(admin_key.objects.count(), 1)
-        self.assertEqual(self.admin_key.key, 'admin_key1')
+    def test_student_password_creation(self):
+        """ Test Case 8.1: Verify creation of student_password entry """
+        self.assertEqual(student_password.objects.count(), 1)
     
-    def test_admin_key_fields(self):
-        """ Test Case 7.2: Check optional fields """
-        self.assertIsNone(self.admin_key.entered_courses)
+    def test_student_password_retrieval(self):
+        """ Test Case 8.3: Retrieve password entry """
+        entry = student_password.objects.get(username='student1')
+        self.assertEqual(entry.password, 'pass1')
+
